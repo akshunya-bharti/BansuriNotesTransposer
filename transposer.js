e5313e2e499e966d0312f2a571eeacb027e0f27c
Akshunya <akshu@live.in>
2017-04-15 12:49:14 +0530

    Used Regex to identify the octave of the note

--- transposer.js
+++ transposer.js
@@ -46,7 +46,48 @@ btnTranspose.onclick = function(){
 	var textToPrint = txtAreaInput.value;
 	selectedNotesPara.innerText = "Selected Notes: ";
 	selectedNotesPara.innerText += textToPrint;
+	transposer(textToPrint);
 }
 
 
+function transposer(selectedNotes){
+	var regExpHigher = /[SrRgGMm]\^/g;
+	var regExpNormal = /[SrRgGMmPdDnN]\-/g;
+	var regExpLower = /[PdDnN]\_/g;
+	var match, matches = [];
 
+	for(var i=0; i<selectedNotes.length; i+=2){
+		var note=selectedNotes.substring(i,i+2);
+		var isHigherNote = Boolean((regExpHigher.exec(note)) != null);
+		var isNormalNote = Boolean((regExpNormal.exec(note)) != null);
+		var isLowerNote = Boolean((regExpLower.exec(note)) != null);
+
+		console.log(note);
+
+
+		// var matchOrNot = (vrbl === true ? "Match" : "Not a Match");
+		// console.log(matchOrNot);
+
+		if(isHigherNote){
+			console.log("Higher");
+		}
+		else if(isNormalNote){
+			console.log("Normal");
+		}
+		else if(isLowerNote){
+			console.log("Lower");
+		}
+
+		isLowerNote=false;
+		isNormalNote=false;
+		isHigherNote=false;
+	}
+
+
+	// while ((match = regExpHigher.exec(selectedNotes)) != null) {
+	//   matches.push(match[0]);
+	//   console.log(match[0]);
+	// }
+
+	// console.log(matches);
+}
